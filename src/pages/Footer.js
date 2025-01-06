0550c5af322f00079fc14dd1b796f5482b444500
PopovicJovan <jovan6.p@gmail.com>
2025-01-06 21:18:52 +0100

    make footer responsive

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -2,33 +2,34 @@ import Button from "react-bootstrap/Button";
 
 const Footer = () => {
     return (
-        <footer>
-            <div id="news-letters" className="my-2 w-75 h-25 px-5 py-1 d-flex justify-content-center align-items-center">
+        <footer className="py-4">
+            <div id="news-letters" className="my-2 w-100  px-sm-5 py-1 d-flex justify-content-center align-items-center flex-column flex-md-row">
                 <div>
-                    <h2>Sign up to our newsletter</h2>
+                    <h2 className="text-center">Sign up to our newsletter</h2>
                 </div>
-                <div className="d-flex w-50 justify-content-end">
-                    <input type="email" className="form-control w-50 mx-1" placeholder="input your email"/>
+                <div className="email-input-footer w-50 d-flex flex-column flex-sm-row justify-content-center align-items-center">
+                    <input type="email" className="form-control mx-1 w-75" placeholder="input your email"/>
                     <Button variant="light">Subscribe</Button>
                 </div>
             </div>
-            <div id="news-letters" className="w-75 h-75 d-flex justify-content-between">
+            <div id="news-letters" className="w-75 h-75 d-flex flex-column flex-md-row justify-content-between">
                 <div className="w-50">
-                    <h2>About us</h2>
-                    <p>
+                    <h2 className="text-center">About us</h2>
+                    <p style={{textAlign: "justify"}}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Vivamus lacinia odio vitae vestibulum. Nulla facilisi.
                         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                         accusantium doloremque laudantium. Ut enim ad minim veniam, quis
                         nostrud exercitation ullamco laboris nisi
-                        ut aliquip ex ea commodo consequat.
+                        ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vivamus lacinia odio.
                     </p>
                 </div>
                 <div className="w-50">
-                    <h2 className="text-danger font-bold text-center">Contact</h2>
-                    <div className="w-100 d-flex flex-column align-items-end">
-                        <ul className="mt-4 position ">
-                            <li>📍 London, New York, Dubai, Athens, Shangai</li>
+                    <h2 className="font-bold text-center">Contact</h2>
+                    <div className="w-100 d-flex flex-column align-items-center">
+                        <ul>
+                            <li>📍 London, New York, Dubai, Athens</li>
                             <li>📞 +1 (212) 555-1234</li>
                             <li>💬 WhatsApp | Telegram | Viber</li>
                             <li>✉️ info@yachtsell.com</li>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -26,16 +26,16 @@ const Main = () => {
             <Navbar/>
             <div className="main-bg">
                 <div className="h-100 w-100 d-flex justify-content-flex align-items-start px-5">
-                <div id="description" className="w-50 h-50 pt-5 d-flex flex-column justify-content-center">
-                    <h2 className="main-page-heading">Elegance on Water</h2>
-                    <h5 style={{color: "#800000", width: "80%"}}>
-                        Embark on a journey like no other with our exclusive fleet of yachts.
-                        Each vessel is designed to offer unparalleled comfort and sophistication.
-                        Discover the freedom of luxury on the open water.
-                    </h5>
-                    <Button href="/yachts" className="w-50"><h5>View our yachts</h5></Button>
+                    <div id="description" className="w-50 h-50 pt-5 d-flex flex-column justify-content-center">
+                        <h2 className="main-page-heading">Elegance on Water</h2>
+                        <h5 style={{color: "#800000", width: "80%"}}>
+                            Embark on a journey like no other with our exclusive fleet of yachts.
+                            Each vessel is designed to offer unparalleled comfort and sophistication.
+                            Discover the freedom of luxury on the open water.
+                        </h5>
+                        <Button href="/yachts" className="w-50"><h5>View our yachts</h5></Button>
+                    </div>
                 </div>
-            </div>
             </div>
             <div id="offer" className="pb-3">
                 <h2 className="text-center fw-bold py-2">The best of the offer</h2>
